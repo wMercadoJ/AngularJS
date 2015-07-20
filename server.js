0bf1696134c33ad83965230f7d38db0d45857a41
wMercadoJ <soi.Luffy@gmail.com>
2015-07-20 08:31:19 -0400

    Config Package

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 //dependencias de las librerias/modulos
-var express = require('express');
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var mongoose       = require('mongoose');
+
 var app = express();
-var mongoose = require('mongoose');
+
 var port = process.env.PORT || 3000;
 
 //conexion a la base de datos
 mongoose.connect("mongodb://localhost:27017/comercial_srvj");
 
-app.configure(function () {
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-});
 
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
 //GET metodo para ir a la pagina principal
-
 require('./public/controllers/routes.js')(app);
+
 //la applicacion escuchara en el puerto 3000
 
 app.listen(port);
